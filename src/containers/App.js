c3ef912eb24c71b7ba3c616ce1d1b9e98f801c5e
IrynaSribna <iryna.sribna@zanox.com>
2018-03-31 12:39:25 +0200

    Component Creation Lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,19 @@ import Persons from'../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] inside constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[App.js] inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] inside componentDidMount()');
+  }
+
   state = {
     persons: [
       {id: '1',   name: 'Eric', age: '2'},
@@ -49,6 +62,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] inside render()');
     let persons = null;
     
     if (this.state.showPersons) {
